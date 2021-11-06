64a0414d8a80d8f39916415ad258a1d2cc81c558
Gabriel Reinhardt <62663953+gabriel3c@users.noreply.github.com>
2021-11-06 11:12:47 -0300

    Update client.js

--- src/provider/client.js
+++ src/provider/client.js
@@ -6,8 +6,4 @@ const client = axios.create({
   baseURL: `https://otterwise-fake-api.herokuapp.com/tasks/${apiCode}`
 })
 
-// email: gabrielmreinhardt@gmail.com
-// senha: jHkZyG
-// api_code: 07fyjcl,
-
 export default client
